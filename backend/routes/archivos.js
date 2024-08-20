1a788a32ef5f48142572e7d9ee12ce43e51d1cac
proyectos-developer <proyectos@developer-ideas.com>
2024-08-20 14:46:03 -0500

    V1.0.0

--- backend/routes/archivos.js
+++ backend/routes/archivos.js
@@ -4,9 +4,17 @@ const router = express.Router()
 const pool = require('../database')
 const { isLoggedIn } = require('../lib/auth')
 
+const fs = require ('fs')
+
 router.post ('/api/upload/foto/:carpeta', (req, res) => {
     const {carpeta} = req.params
     const file = req.files.file
+    if (fs.existsSync(`./${carpeta}`)){
+        console.log ('existe')
+    }
+    else{
+        console.log ('no existe')
+    }
     file.mv(`./${carpeta}/${file.name}`, err => {
         if (err){ 
             return res.json({
