fbe72e45058cb6e0b52c5b72d93fcc3eed141fc1
proyectos-developer <proyectos@developer-ideas.com>
2024-08-21 20:06:53 -0500

    V1.0.0

--- backend/routes/compras.js
+++ backend/routes/compras.js
@@ -5,15 +5,21 @@ const pool = require('../database')
 const { isLoggedIn } = require('../lib/auth')
 
 router.post ('/api/compras', async (req, res) => {
-    const {id_producto, usuario, cantidad, precio_unidad, precio_total, shop_id} = req.body
+    const {id_producto, usuario, cantidad, precio_unidad, precio_total, shop_id,
+        producto, descripcion, url_foto, id_categoria, categoria, id_sub_categoria, sub_categoria,
+        id_unidad, unidad, id_servicio, servicio
+    } = req.body
 
     try {
-        const newCompra = {id_producto, usuario, cantidad, precio_unidad, precio_total, shop_id}
-        await pool.query ('INSERT INTO compras set ?', [newCompra])
-        const compras = await pool.query ('SELECT * FROM compras WHERE shop_id = ?', [shop_id])
+        const newCompra = {id_producto, usuario, cantidad, precio_unidad, precio_total, shop_id,
+            producto, descripcion, url_foto, id_categoria, categoria, id_sub_categoria, sub_categoria,
+            id_unidad, unidad, id_servicio, servicio
+        }
+        const new_compra = await pool.query ('INSERT INTO compras set ?', [newCompra])
+        const compras = await pool.query ('SELECT * FROM compras WHERE id = ?', [new_compra.insertId])
 
         return res.json ({
-            compras: compras,
+            compra: compras[0],
             success: true
         })
     } catch (error) {
@@ -27,16 +33,16 @@ router.post ('/api/compras', async (req, res) => {
 })
 
 router.post ('/api/compras/:id_compra', async (req, res) => {
-    const {id_producto, usuario, cantidad, precio_unidad, precio_total} = req.body
+    const {cantidad, precio_unidad, precio_total} = req.body
     const {id_compra} = req.params
 
     try {
-        const updateCompra = {id_producto, usuario, cantidad, precio_unidad, precio_total}
+        const updateCompra = {cantidad, precio_unidad, precio_total}
         await pool.query ('UPDATE compras set ? WHERE id = ?', [updateCompra, id_compra])
         const compras = await pool.query ('SELECT * FROM compras WHERE id = ?', [id_compra])
 
         return res.json ({
-            compras: compras,
+            compra: compras[0],
             success: true
         })
     } catch (error) {
@@ -49,23 +55,85 @@ router.post ('/api/compras/:id_compra', async (req, res) => {
     }
 })
 
-router.get ('/api/compras/:begin/:amount', async (req, res) => {
-    const {begin, amount} = req.params
+router.get ('/api/compras/search/:search/order_by/:order_by/:order/:begin/:amount', async (req, res) => {
+    const {search, order_by, order, begin, amount} = req.params
     try {
-        const compras = await pool.query (`SELECT * FROM compras GROUP BY shop_id LIMIT ${begin},${amount}`)
-        if (parseInt(begin) === 0){
-            const total_compras = await pool.query ('SELECT COUNT (id) FROM compras GROUP BY shop_id')
-
-            return res.json ({
-                total_compras: total_compras[0][`COUNT (id)`],
-                compras: compras,
-                success: true
-            })
-        }else{
-            return res.json ({
-                compras: compras,
-                success: true
-            })
+        if (search === '0' && order_by === '0'){
+            const compras = await pool.query (`SELECT * FROM compras GROUP BY shop_id LIMIT ${begin},${amount}`)
+            if (parseInt(begin) === 0){
+                const total_compras = await pool.query ('SELECT COUNT (id) FROM compras')
+    
+                return res.json ({
+                    total_compras: total_compras[0][`COUNT (id)`],
+                    compras: compras,
+                    success: true
+                })
+            }else{
+                return res.json ({
+                    compras: compras,
+                    success: true
+                })
+            }
+        }else if (search === '0' && order_by !== '0'){
+            const compras = await pool.query (`SELECT * FROM compras GROUP BY shop_id ORDER BY 
+                    ${order_by} ${order} LIMIT ${begin},${amount}`)
+            if (parseInt(begin) === 0){
+                const total_compras = await pool.query ('SELECT COUNT (id) FROM compras')
+    
+                return res.json ({
+                    total_compras: total_compras[0][`COUNT (id)`],
+                    compras: compras,
+                    success: true
+                })
+            }else{
+                return res.json ({
+                    compras: compras,
+                    success: true
+                })
+            }
+        }else if (search !== '0' && order_by === '0'){
+            const compras = await pool.query (`SELECT * FROM compras WHERE (producto LIKE '%${search}%' OR
+                descripcion LIKE '%${search}%' OR categoria LIKE '%${search}%' OR sub_categoria
+                LIKE '%${search}%' OR unidad LIKE '%${search}%') GROUP BY shop_id LIMIT ${begin},${amount}`)
+            if (parseInt(begin) === 0){
+                const total_compras = await pool.query (`SELECT COUNT (id) FROM compras 
+                    WHERE (producto LIKE '%${search}%' OR
+                descripcion LIKE '%${search}%' OR categoria LIKE '%${search}%' OR sub_categoria
+                LIKE '%${search}%' OR unidad LIKE '%${search}%')`)
+    
+                return res.json ({
+                    total_compras: total_compras[0][`COUNT (id)`],
+                    compras: compras,
+                    success: true
+                })
+            }else{
+                return res.json ({
+                    compras: compras,
+                    success: true
+                })
+            }
+        }else if (search !== '0' && order_by !== '0'){
+            const compras = await pool.query (`SELECT * FROM compras WHERE (producto LIKE '%${search}%' OR
+                descripcion LIKE '%${search}%' OR categoria LIKE '%${search}%' OR sub_categoria
+                LIKE '%${search}%' OR unidad LIKE '%${search}%') GROUP BY shop_id
+                ORDER BY ${order_by} ${order} LIMIT ${begin},${amount}`)
+            if (parseInt(begin) === 0){
+                const total_compras = await pool.query (`SELECT COUNT (id) FROM compras 
+                    WHERE (producto LIKE '%${search}%' OR
+                descripcion LIKE '%${search}%' OR categoria LIKE '%${search}%' OR sub_categoria
+                LIKE '%${search}%' OR unidad LIKE '%${search}%')`)
+    
+                return res.json ({
+                    total_compras: total_compras[0][`COUNT (id)`],
+                    compras: compras,
+                    success: true
+                })
+            }else{
+                return res.json ({
+                    compras: compras,
+                    success: true
+                })
+            }
         }
     } catch (error) {
         console.log (error)
@@ -106,7 +174,7 @@ router.get ('/api/compras/productos/:shop_id/:begin/:amount', async (req, res) =
     }
 })
 
-router.get ('/api/delete/compras/:id_carrito/:shop_id', async (req, res) => {
+router.get ('/api/delete/producto/compra/:id_carrito/:shop_id', async (req, res) => {
     const {id_carrito, shop_id} = req.params
 
     try {
